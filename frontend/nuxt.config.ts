dbc1895c6b34d43d926664c7a6514fc0f7bc9509
Kwizera Sezerano <tabitakwizerisezerano@gmail.com>
2026-06-23 14:32:07 +0200

    ft:overall system translations

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'TB Diagnostic System',
+      title: 'TB',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
